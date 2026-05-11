f19870c4a085a67802fd413f520cc883831c7769
ThaaCoree <108455880+ThaaCoree@users.noreply.github.com>
2026-05-11 19:45:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ client.on('messageCreate', async (message) => {
     const username = message.author.username;
     const roles = message.member?.roles.cache.map(r => r.name); // ['Admin', 'Member', ...]
     const args = message.content.split(' ');
-    const message = args[1];
+    const messageContent = args[1];
 
     try {
       const res = await fetch('https://lastopus-discord-service-production.up.railway.app/equip', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ roles, message }) // ส่งไปกับ request ด้วยได้
+        body: JSON.stringify({ roles, message: messageContent }) // ส่งไปกับ request ด้วยได้
       });
     
       const data = await res.text();
